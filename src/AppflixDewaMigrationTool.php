9ff663ac89a6f2a6ae690414ef477a4ae684e03c
moori <info@moorleiche.com>
2021-09-23 18:28:06 +0200

    dev

--- src/AppflixDewaMigrationTool.php
+++ src/AppflixDewaMigrationTool.php
@@ -2,15 +2,12 @@
 
 namespace Appflix\DewaMigrationTool;
 
-use Appflix\Foundation\Core\PluginTrait;
 use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 
 class AppflixDewaMigrationTool extends Plugin
 {
-    use PluginTrait;
-
     public const NAME = 'AppflixDewaMigrationTool';
     public const DATA_CREATED_AT = '2001-02-03 01:02:04.000';
     public const PLUGIN_TABLES = ['dewa_migration'];
@@ -23,10 +20,16 @@ class AppflixDewaMigrationTool extends Plugin
             return;
         }
 
-        $this->uninstallTrait(
-            self::PLUGIN_TABLES,
-            null,
-            self::DATA_CREATED_AT
-        );
+        $this->uninstallTrait();
+    }
+
+    private function uninstallTrait(): void
+    {
+        $connection = $this->container->get(Connection::class);
+
+        foreach (self::PLUGIN_TABLES as $table) {
+            $sql = sprintf('SET FOREIGN_KEY_CHECKS=0; DROP TABLE IF EXISTS `%s`;', $table);
+            $connection->executeUpdate($sql);
+        }
     }
 }
